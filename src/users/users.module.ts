ed627e3cd0517477347be601b16e8cb321acb39d
Mahmoud samir <mahmoudsamirsayed97@gmail.com>
2026-07-11 17:47:25 +0300

    refactor(users): resolve current user via middleware
    Replace the global CurrentUserInterceptor with a CurrentUserMiddleware
    applied to all routes, populating req.currentUser from the session.
    Add an Express Request type augmentation to type the currentUser
    property.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,22 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/user.entity';
 import { AuthService } from 'src/users/auth.service';
-import { CurrentUserInterceptor } from 'src/users/interceptors/current-user.interceptor';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CurrentUserMiddleware } from '@/users/middlewares/current-user-middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [
-    UsersService,
-    AuthService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CurrentUserInterceptor,
-    },
-  ],
+  providers: [UsersService, AuthService],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
